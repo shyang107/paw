37063f899a76cc19c7606267c1b27f728594a822
YANG SHUH HUA <shyang107@gmail.com>
2020-11-12 16:19:57 +0800

    add NewRand, some sort[Func]

--- ex/main.go
+++ ex/main.go
@@ -37,7 +37,54 @@ func main() {
 	// exReverse()
 	// exPrintTree()
 	// exShuffle()
-	exGetCurrPath()
+	// exGetCurrPath()
+	var n1 = []int{1, 39, 2, 9, 7, 54, 11}
+	var n2 = []int{1, 39, 2, 9, 7, 54, 11}
+	// var n = []int{4, 3, 2, 10, 12, 1, 5, 6}
+	InsertionSort(n1)
+	SelectionSort(n2)
+}
+
+// SelectionSort 選擇排序
+func SelectionSort(n []int) {
+	fmt.Println(n)
+	// count := 0
+	// for i := 0; i < len(n); i++ {
+	// 	minIndex := i
+	// 	for j := i + 1; j < len(n); j++ {
+	// 		if n[minIndex] > n[j] {
+	// 			minIndex = j
+	// 		}
+	// 	}
+	// 	n[i], n[minIndex] = n[minIndex], n[i]
+	// 	count++
+	// 	fmt.Println(count, n)
+	// }
+	paw.SelectionSort(n)
+	fmt.Println(n)
+	paw.SelectionSortFunc(n, func(a, b int) bool { return a < b })
+	fmt.Println(n)
+}
+
+// InsertionSort 插入排序
+func InsertionSort(n []int) {
+	fmt.Println(n)
+	// count := 0
+	// i := 1
+	// for i < len(a) {
+	// 	j := i
+	// 	for j >= 1 && a[j] < a[j-1] {
+	// 		a[j-1], a[j] = a[j], a[j-1]
+	// 		count++
+	// 		fmt.Println(count, a)
+	// 		j--
+	// 	}
+	// 	i++
+	// }
+	paw.InsertionSort(n)
+	fmt.Println(n)
+	paw.InsertionSortFunc(n, func(a, b int) bool { return a < b })
+	fmt.Println(n)
 }
 func exGetCurrPath() {
 	fmt.Println(paw.GetCurrPath())
--- helpers.go
+++ helpers.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+// NewRand return a instance of
+func NewRand() *rand.Rand {
+	return rand.New(rand.NewSource(time.Now().Unix()))
+}
+
 // Shuffle randomly shuffle the order of `slice`
 func Shuffle(slice []interface{}) {
 	r := rand.New(rand.NewSource(time.Now().Unix()))
@@ -13,5 +18,65 @@ func Shuffle(slice []interface{}) {
 		randIndex := r.Intn(n)
 		slice[n-1], slice[randIndex] = slice[randIndex], slice[n-1]
 		slice = slice[:n-1]
+	}
+}
+
+// SelectionSort 選擇排序 (升冪) (較慢)
+func SelectionSort(n []int) {
+	for i := 0; i < len(n); i++ {
+		minIndex := i
+		for j := i + 1; j < len(n); j++ {
+			if n[minIndex] > n[j] {
+				minIndex = j
+			}
+		}
+		n[i], n[minIndex] = n[minIndex], n[i]
+	}
+}
+
+// SelectionSortFunc 選擇排序 (升冪) (較慢)
+// 	f(a,b) : true for exchange, false not
+// Example:
+// 	升冪:  f func(a, b int) bool { rerturn a > b}
+// 	降冪:  f func(a, b int) bool { rerturn a < b}
+func SelectionSortFunc(n []int, f func(a, b int) bool) {
+	for i := 0; i < len(n); i++ {
+		minIndex := i
+		for j := i + 1; j < len(n); j++ {
+			if f(n[minIndex], n[j]) {
+				minIndex = j
+			}
+		}
+		n[i], n[minIndex] = n[minIndex], n[i]
+	}
+}
+
+// InsertionSort 插入排序 (升冪) (較快)
+func InsertionSort(n []int) {
+	i := 1
+	for i < len(n) {
+		j := i
+		for j >= 1 && n[j-1] > n[j] {
+			n[j-1], n[j] = n[j], n[j-1]
+			j--
+		}
+		i++
+	}
+}
+
+// InsertionSortFunc 插入排序 (升冪) (較快)
+// 	f(a,b) : true for exchange, false not
+// Example:
+// 	升冪:  f func(a, b int) bool { rerturn a > b}
+// 	降冪:  f func(a, b int) bool { rerturn a < b}
+func InsertionSortFunc(n []int, f func(a, b int) bool) {
+	i := 1
+	for i < len(n) {
+		j := i
+		for j >= 1 && f(n[j-1], n[j]) {
+			n[j-1], n[j] = n[j], n[j-1]
+			j--
+		}
+		i++
 	}
 }
