267f6940fcbdabbcdf6136cfc2d7bae184e4116a
YANG SHUH HUA <shyang107@gmail.com>
2020-11-27 23:34:24 +0800

    add comment

--- helpers.go
+++ helpers.go
@@ -13,7 +13,11 @@ func GetFuncName(level interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(level).Pointer()).Name()
 }
 
-// GetDate get the current date and return string
+// GetDate get the current date and return string. For example,
+//	GetDate() return carbon.Now().Format(carbon.FormattedDateFormat)
+//	if time.Now().Date() is `2020 November 27`
+//		carbon.FormattedDateFormat = "Jan 2, 2006"
+//	then GetDate() return `Nov 27, 2020`
 func GetDate() string {
 	// y, m, d := time.Now().Date()
 	// ms := string(([]byte(m.String()))[0:3])
